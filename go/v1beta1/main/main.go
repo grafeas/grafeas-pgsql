9e590d7514036fc21487c7c1f1abd0c9df3b1261
Brendin Nye <brendin@brendin.net>
2022-06-28 11:00:26 -0500

    rename storage provider func name to please the linter. Add main file to register storage from this repo with grafeas so it is used.

---
+++ go/v1beta1/main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/grafeas/grafeas/go/v1beta1/server"
+	grafeasStorage "github.com/grafeas/grafeas/go/v1beta1/storage"
+
+	"github.com/grafeas/grafeas-pgsql/go/v1beta1/storage"
+)
+
+func main() {
+	err := grafeasStorage.RegisterStorageTypeProvider("postgres", storage.PostgresqlStorageTypeProvider)
+	if err != nil {
+		log.Panicf("Error when registering postgres storage provider, %s", err)
+	}
+
+	err = server.StartGrafeas()
+	if err != nil {
+		log.Fatalf("Failed to start Grafeas server, %s", err)
+	}
+}
